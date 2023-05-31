46d122e45d559a27d6b8f7cb15288959e0fd1815
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2023-05-31 16:16:56 +0700

    Change file location

--- script/install.sh
+++ script/install.sh
@@ -5,6 +5,8 @@ PROJECT=$DIR/..
 
 go build -o $PROJECT/latinaserver $PROJECT/cmd/latinaserver/main.go
 
+sudo mkdir -p /usr/local/etc/latinaserver
+sudo cp $PROJECT/config.json /usr/local/etc/latinaserver/
 sudo cp $PROJECT/latinaserver /usr/local/bin/ 
 sudo cp ./latinaserver.service /etc/systemd/system/
 sudo systemctl daemon-reload
--- script/uninstall.sh
+++ script/uninstall.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sudo systemctl stop latinaserver
+sudo rm -rf /usr/local/etc/latinaserver
 sudo rm -rf /usr/local/bin/latinaserver
 sudo rm -rf /etc/systemd/system/latinaserver.service
 sudo systemctl daemon-reload
