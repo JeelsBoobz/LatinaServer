275ab45069ed78e1931d29d08b7954c9192fd181
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2023-06-06 11:20:02 +0700

    Fix: Service helper not reloading

--- helper/service.go
+++ helper/service.go
@@ -3,22 +3,18 @@ package helper
 import (
 	"fmt"
 	"os/exec"
-	"regexp"
-	"strings"
 )
 
 func ReloadService(names ...string) {
 	for _, name := range names {
 		fmt.Println("Reloading", name, "...")
-		out, err := exec.Command("systemctl", "reload", name).Output()
-		var ignoredText = []string{
-			"reload.",
-			"inactive.",
-		}
-		if m, _ := regexp.MatchString(regexp.MustCompile(strings.Join(ignoredText[:], "|")).String(), string(out)); m {
-			exec.Command("systemctl", "start", name)
-		} else if err != nil {
-			panic(err)
+		_, err := exec.Command("systemctl", "reload", name).Output()
+		if err != nil {
+			if err.Error() == "exit status 1" {
+				exec.Command("systemctl", "restart", name)
+			} else {
+				panic(err)
+			}
 		}
 
 		fmt.Println(name, "successfully reloaded !")
--- script/install.sh
+++ script/install.sh
@@ -8,9 +8,13 @@ go build -o $PROJECT/latinaserver $PROJECT/cmd/latinaserver/main.go
 sudo mkdir -p /usr/local/etc/latinaserver
 sudo cp $PROJECT/resources/openresty/stream/reverse_default.conf /usr/local/etc/latinaserver/
 sudo cp $PROJECT/config.json /usr/local/etc/latinaserver/
-sudo cp $PROJECT/latinaserver /usr/local/bin/ 
 
 if [ ! -f /etc/systemd/system/latinaserver.service ]; then
     sudo cp ./latinaserver.service /etc/systemd/system/
+else 
+    sudo systemctl stop latinaserver
 fi
+sudo cp $PROJECT/latinaserver /usr/local/bin/
 sudo systemctl daemon-reload
+
+sudo systemctl start latinaserver
