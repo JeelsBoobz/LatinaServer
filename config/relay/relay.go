ae7d823a58724ee9b80c472b684585ac342324b1
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2023-06-11 21:28:10 +0700

    Limit relay server

--- config/relay/relay.go
+++ config/relay/relay.go
@@ -18,18 +18,20 @@ func GetRelayOutbounds() []option.Outbound {
 	supabase.Connect().DB.From("proxies").Select("*").Eq("vpn", "shadowsocks").Neq("country_code", helper.GetIpInfo().CountryCode).Execute(&proxies)
 
 	for _, proxy := range proxies {
-		outboundsMap[proxy.CountryCode] = append(outboundsMap[proxy.CountryCode], option.Outbound{
-			Tag:  proxy.Remark,
-			Type: proxy.VPN,
-			ShadowsocksOptions: option.ShadowsocksOutboundOptions{
-				ServerOptions: option.ServerOptions{
-					Server:     proxy.Server,
-					ServerPort: uint16(proxy.ServerPort),
+		if len(outboundsMap[proxy.CountryCode]) < 3 {
+			outboundsMap[proxy.CountryCode] = append(outboundsMap[proxy.CountryCode], option.Outbound{
+				Tag:  proxy.Remark,
+				Type: proxy.VPN,
+				ShadowsocksOptions: option.ShadowsocksOutboundOptions{
+					ServerOptions: option.ServerOptions{
+						Server:     proxy.Server,
+						ServerPort: uint16(proxy.ServerPort),
+					},
+					Method:   proxy.Method,
+					Password: proxy.Password,
 				},
-				Method:   proxy.Method,
-				Password: proxy.Password,
-			},
-		})
+			})
+		}
 	}
 
 	for cc, out := range outboundsMap {
